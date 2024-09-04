4d692e447e7c1b05f03790f304b583e5c1aa87fd
Jinwoo Cho <133188752+jinoo0306@users.noreply.github.com>
2024-09-04 20:15:37 +0900

    [Bronze V] Title: A/B, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const inputData = fs
+  //   .readFileSync("input.txt", "utf8")
+  .readFileSync("/dev/stdin", "utf8")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((value) => parseInt(value));
+
+const [a, b] = inputData;
+
+console.log(a / b);
