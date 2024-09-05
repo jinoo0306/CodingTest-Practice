eaace3b15653bfc0b084ea05e0b9bb6facbe63d3
Jinwoo Cho <133188752+jinoo0306@users.noreply.github.com>
2024-09-05 21:06:07 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const inputData = fs
+  // .readFileSync("input.txt", "utf8")
+  .readFileSync("/dev/stdin", "utf8")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((value) => parseInt(value));
+
+const [a, b] = inputData;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
