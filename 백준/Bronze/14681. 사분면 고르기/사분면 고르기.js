04a6b92612bf8341349f233f89901b650f975cd8
Jinwoo Cho <133188752+jinoo0306@users.noreply.github.com>
2024-09-08 22:50:57 +0900

    [Bronze V] Title: 사분면 고르기, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const inputData = fs
+  // .readFileSync("input.txt", "utf8")
+  .readFileSync(0, "utf8")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [a, b] = inputData.map(Number);
+
+function findQuadrant(x, y) {
+  if (x > 0 && y > 0) {
+    return 1;
+  } else if (x < 0 && y > 0) {
+    return 2;
+  } else if (x < 0 && y < 0) {
+    return 3;
+  } else if (x > 0 && y < 0) {
+    return 4;
+  }
+}
+
+console.log(findQuadrant(a, b));
