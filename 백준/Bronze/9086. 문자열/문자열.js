f0986cef123b7db0c158b2d15b9e333ca340c9ba
Jinwoo Cho <133188752+jinoo0306@users.noreply.github.com>
2024-09-14 20:01:14 +0900

    [Bronze V] Title: 문자열, Time: 84 ms, Memory: 9316 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.js
+++ 백준/Bronze/9086. 문자열/문자열.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
+const inputData = fs
+  // .readFileSync("input.txt", "utf8")
+  .readFileSync(0, "utf8")
+  .toString()
+  .trim()
+  .split("\n");
 
-// 입력을 읽어옴
-const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+const T = parseInt(inputData[0]);
+const result = [];
 
-// 첫 번째 줄은 테스트 케이스 개수
-const T = parseInt(input[0]);
-
-for (let i = 1; i <= T; i++) {
-  const str = input[i];
-  const firstChar = str[0];
-  const lastChar = str[str.length - 1];
-  
-  // 첫 번째와 마지막 문자를 출력
-  console.log(`${firstChar}${lastChar}`);
+for (i = 1; i < T + 1; i++) {
+  result.push(`${inputData[i][0]}${inputData[i][inputData[i].length - 1]}`);
 }
+
+console.log(result.join("\n"));
