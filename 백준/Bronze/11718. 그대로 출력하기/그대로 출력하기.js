a9d34322356a53813466359b2dcabbe4c9eeec25
Jinwoo Cho <133188752+jinoo0306@users.noreply.github.com>
2024-09-15 20:08:49 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const inputData = fs
+  // .readFileSync("input.txt", "utf8")
+  .readFileSync(0, "utf8")
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log(inputData.join("\n"));
