22da7a261b92c0b91c7f7af67161a1c0cecb2f1f
Jinwoo Cho <133188752+jinoo0306@users.noreply.github.com>
2024-10-17 23:07:26 +0900

    [Bronze V] Title: 직사각형, Time: 92 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const inputData = fs
+  // .readFileSync("input.txt", "utf8")
+  .readFileSync("/dev/stdin", "utf8")
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log(inputData[0]*inputData[1]);
