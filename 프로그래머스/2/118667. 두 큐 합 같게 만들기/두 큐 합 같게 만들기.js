6707d223955c9fc64a5b1d2963c8dfce4973c445
Jinoo <133188752+jinoo0306@users.noreply.github.com>
2025-03-30 22:35:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 37.01 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,30 +1,4 @@
 function solution(queue1, queue2) {
-  const totalLength = queue1.length + queue2.length;
-  const merged = [...queue1, ...queue2];
-
-  let sum1 = queue1.reduce((a, b) => a + b, 0);
-  let sum2 = queue2.reduce((a, b) => a + b, 0);
-  const target = (sum1 + sum2) / 2;
-
-  if ((sum1 + sum2) % 2 !== 0) return -1;
-
-  let p1 = 0; 
-  let p2 = queue1.length; 
-  let moveCount = 0;
-
-  while (moveCount <= totalLength * 3) {
-    if (sum1 === target) return moveCount;
-
-    if (sum1 > target) {
-      sum1 -= merged[p1];
-      p1++;
-    } else {
-      sum1 += merged[p2];
-      p2++;
-    }
-
-    moveCount++;
-  }
-
-  return -1;
+    var answer = -2;
+    return answer;
 }
