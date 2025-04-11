6dc59a702294ba561dea0987fff298e0ec5f89a3
Jinoo <133188752+jinoo0306@users.noreply.github.com>
2025-04-11 14:43:40 +0900

    [level 3] Title: 등굣길, Time: 1.09 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,22 +1,4 @@
 function solution(m, n, puddles) {
-  const dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
-
-  for (let [x, y] of puddles) {
-    dp[y][x] = -1;
-  }
-
-  dp[1][1] = 1;
-
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= m; j++) {
-      if (i === 1 && j === 1) continue;
-      if (dp[i][j] === -1) {
-        dp[i][j] = 0;
-      } else {
-        dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
-      }
-    }
-  }
-
-  return dp[n][m];
+    var answer = 0;
+    return answer;
 }
