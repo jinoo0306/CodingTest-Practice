555850739fd298a2ef236b19a040a7200a894f70
Jinoo <133188752+jinoo0306@users.noreply.github.com>
2025-04-14 19:39:58 +0900

    [level 2] Title: 삼각 달팽이, Time: 32.35 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,25 +1,4 @@
 function solution(n) {
-  const snail = Array.from({ length: n }, (_, i) => Array(i + 1).fill(0));
-  let num = 1;
-  let x = -1,
-    y = 0;
-
-  for (let i = 0; i < n; i++) {
-    for (let j = i; j < n; j++) {
-      if (i % 3 === 0) x++;
-      else if (i % 3 === 1) y++;
-      else if (i % 3 === 2) {
-        x--;
-        y--;
-      }
-      snail[x][y] = num++;
-    }
-  }
-
-  const result = [];
-  for (let row of snail) {
-    for (let val of row) result.push(val);
-  }
-
-  return result;
+    var answer = [];
+    return answer;
 }
